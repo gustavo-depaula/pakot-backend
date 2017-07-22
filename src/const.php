837619f695e5501a5b0798bda2b5621a28e11cb4
Unknown <raulfmansur@hotmail.com>
2017-07-22 17:31:58 -0300

    arquivo de constantes

---
+++ src/const.php
@@ -0,0 +1,6 @@
+<?php
+	define("DB_HOST", "us-cdbr-iron-east-03.cleardb.net:3306");
+	define("DB_USER", "bd3294cb43e05c");
+	define("DB_PASSWORD", "07b0f1c3");
+	define("DB_NAME", "heroku_cacf46323b37948");
+?>
