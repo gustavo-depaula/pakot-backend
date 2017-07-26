36c6b16341a4817565a1502710b6161d57137c18
Unknown <raulfmansur@hotmail.com>
2017-07-26 16:17:12 -0300

    All requires located at const.php

--- src/const.php
+++ src/const.php
@@ -1,4 +1,17 @@
 <?php
+	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/classes/users/User.php';
+	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/classes/Package.php';
+
+	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/package/CreatePackage.php';
+	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/package/GetOpenPackages.php';
+	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/package/GetPackage.php';
+
+	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/Login/UserLog.php';
+	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/Login/SignUp.php';
+
+	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/User/getUserData.php';
+	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/User/updateUserData.php';
+
 	define("DB_HOST", "us-cdbr-iron-east-03.cleardb.net:3306");
 	define("DB_USER", "bd3294cb43e05c");
 	define("DB_PASSWORD", "07b0f1c3");
