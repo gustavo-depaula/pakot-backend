2d9635e2c2e0d636c450eaec9bbe8b23480ffef8
Unknown <raulfmansur@hotmail.com>
2017-07-26 16:21:46 -0300

    updateUserData function added.

---
+++ src/pakot/functions/User/updateUserData.php
@@ -0,0 +1,16 @@
+<?php
+	function updateUserData($request, $email){
+		$name = $request['name'];
+		$cpf = $request['cpf'];
+		$phone = $request['phone'];
+		$payment = $request['payment'];
+
+		$conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+		echo $email;
+		$sql = "UPDATE commonuser SET name='$name', cpf='$cpf', phone='$phone', payment='$payment' WHERE email='$email'";
+		$conn->query($sql);
+		$conn->close();
+		return new User($name,$email,$phone,$payment,$cpf);
+	}
+
+?>
