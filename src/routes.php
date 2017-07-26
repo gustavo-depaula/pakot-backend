b8c7360b5befb012ac74a5bab437ad4130475b7f
Unknown <raulfmansur@hotmail.com>
2017-07-26 16:18:24 -0300

    sessions fixed, requires moved, getUserData and updateUserData added.

--- src/routes.php
+++ src/routes.php
@@ -2,8 +2,10 @@
 // Routes
 
 require 'const.php';
+ob_start();  
 
-
+if(!isset($_SESSION))
+    session_start();
 
 $app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
@@ -18,58 +20,80 @@ $app->add(function ($req, $res, $next) {
 });
 
 $app->post('/package/create',function ($request, $response, $args) {
-	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/package/CreatePackage.php';
-
 	$request = $request->getParsedBody();
-
 	$package = createPackage($request);
 	return $package->toJson();
 });
 
 $app->get('/package/getopen',function ($request, $response, $args) {
-	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/package/GetOpenPackages.php';
-
 	$packages = getOpenPackages();
 	return json_encode($packages);
 });
 
 $app->get('/package/getpackage/{id}',function ($request, $response, $args) {
-	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/package/GetPackage.php';
-
 	$package = getPackageById($args['id']);
 	return json_encode($package);
 });
 
 $app->post('/login/User',function ($request, $response, $args){
-	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/Login/UserLog.php';
 	$request = $request->getParsedBody();
-
-	$user = checkUser($request);
-
+	$_SESSION['currentUserEmail'] = $request['email'];
 	// if user exists -> return 'dataFound'
 	// else -> return 'requireSignUp'
-	
-	return json_encode($user);
+	return json_encode(checkUser($_SESSION['currentUserEmail']));
 });
 
 $app->post('/login/SignUp',function ($request, $response, $args){
-	require $_SERVER['DOCUMENT_ROOT'] . '/src/pakot/functions/Login/SignUp.php';
 	$request = $request->getParsedBody();
+	$_SESSION['currentUserEmail'] = $request['email'];
+	return signUp($request);
+});
 
-	signUp($request);
+$app->get('/User/getData',function ($request, $response, $args){
+	if(isset($_SESSION["currentUserEmail"])){
+		$data = getUserData($_SESSION['currentUserEmail']);
+		return json_encode($data);
+	}
+	else
+		return 'noUserLogged';
+});
 
-	return 'SignUpSuccess';
+$app->post('/User/getData',function ($request, $response, $args){
+	if(isset($_SESSION["currentUserEmail"])){
+		$data = getUserData($_SESSION['currentUserEmail']);
+		return json_encode($data);
+	}
+	else
+		return 'noUserLogged';
+});
+
+$app->post('/User/update',function ($request, $response, $args){
+	// if data is updated, a new user object is returned
+	if(isset($_SESSION["currentUserEmail"])){
+		$request = $request->getParsedBody();
+ 		return json_encode(updateUserData($request,$_SESSION['currentUserEmail']));
+	}
+	else
+		return 'noUserLogged';
 });
 
 $app->post('/logoff/User',function ($request, $response, $args){
-	unset($_SESSION["currentUser"]);
+	unset($_SESSION["currentUserEmail"]);
+	session_destroy();
 	return 'LoggedOut';
 });
 
 $app->get('/',function ($request, $response, $args){
-	var_dump($_SESSION['currentUser']);
 	return 'I am working!';
 });
+
+// TO DO
+// calculo de preço a partir de distancia
+
+// pegar pacotes passados de um cliente
+// pegar pacotes em aberto de um cliente
+// pegar pacotes em andamento de um cliente
+
 
 $app->post('/login/DeliveryMan',function ($request, $response, $args){
 	/*require '';
