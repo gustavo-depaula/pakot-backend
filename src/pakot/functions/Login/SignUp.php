b0efc650516c7a4bdd46a6a79e6cfd1c5be70d1f
Unknown <raulfmansur@hotmail.com>
2017-07-26 14:55:14 -0300

    Support to error on sign up

--- src/pakot/functions/Login/SignUp.php
+++ src/pakot/functions/Login/SignUp.php
@@ -7,12 +7,14 @@
 		$cpf = $request['cpf'];
 		$phone = $request['phone'];
 		$payment = $request['payment'];
-		$rating = 'undefined';
 
 		$conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-		$sql = "INSERT INTO commonuser (name,cpf,rating,phone,payment)
-		VALUES ('$name','$cpf','undefined','$phone','$payment')";
-		$conn->query($sql);
+		$sql = "INSERT INTO commonuser (name,email,cpf,rating,phone,payment,packages)
+		VALUES ('$name','$email','$cpf','undefined','$phone','$payment','undefined')";
+		if ($conn->query($sql) === TRUE) 
+			return 'SignUpSuccess';
+		else 
+			return 'SignUpError';
 		$conn->close();
 	}
 
