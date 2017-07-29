1bc2eb1e87c479ebfb3a193f50a1ed5c5b5467de
Unknown <raulfmansur@hotmail.com>
2017-07-29 00:54:27 -0300

    getPackageByStatus function added.

--- src/pakot/functions/package/GetPackage.php
+++ src/pakot/functions/package/GetPackage.php
@@ -1,15 +1,32 @@
 <?php
 	function getPackageById($id){
+		$package;
 		$conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
 		$sql = "SELECT * FROM packages";
 		$result = $conn->query($sql);
 	
 	    while($row = $result->fetch_assoc()) {
-			if($row['id'] == $id)		
+			if($row['id'] == $id){
 				$package = $row;	 
+				$conn->close();
+				return $package;
+			}		
 	    }
-		$conn->close();
-		return $package;
+	    return 'packageNotFound';
+	}
+
+	function getPackagebyStatus($email, $status){
+		// get the logged user packages ids
+		$user = getUser($email);
+
+		// get all the packages by ids
+		$packagesIds = explode(";", $user->packages);
+		$packages = [];
+		for($i=0;$i<count($packagesIds);$i++){
+			array_push($packages, getPackageById($packagesIds[$i]));
+		}
+
+		return $packages;
 	}
 ?>
