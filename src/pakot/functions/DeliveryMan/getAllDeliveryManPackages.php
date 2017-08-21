22e017b378aeb8764e50e3560e1238a22b729c12
Unknown <raulfmansur@hotmail.com>
2017-08-20 23:43:17 -0300

    Bug fix.

--- src/pakot/functions/DeliveryMan/getAllDeliveryManPackages.php
+++ src/pakot/functions/DeliveryMan/getAllDeliveryManPackages.php
@@ -5,13 +5,13 @@ function getAllDeliveryManPackages($email){
 	$packagesIds = explode(";", $deliveryMan->packages);
 
 	$packages = [];
-	for($i=1;$i<count($packagesIds);$i++){
+	for($i=0;$i<count($packagesIds);$i++){
 		$p = getPackageById($packagesIds[$i]);
 		if($p!="packageNotFound"){
 			array_push($packages, getPackageById($packagesIds[$i]));
 			$packages[count($packages)-1]['flag'] = false;
-			$packages[count($packages)-1]['price'] = intval($packages[$i-1]['price']);
-			$packages[count($packages)-1]['deliveryCut'] = $packages[$i-1]['price'] * DELIVERYMANPERCENTAGE;				
+			$packages[count($packages)-1]['price'] = intval($packages[count($packages)-1]['price']);
+			$packages[count($packages)-1]['deliveryCut'] = $packages[count($packages)-1]['price'] * DELIVERYMANPERCENTAGE;				
 		}
 	}
 	return array_reverse($packages);
