26dd2a8202074f344302b54ac98cf42afeeb0cc4
Unknown <raulfmansur@hotmail.com>
2017-09-01 15:59:07 -0300

    bug fix.

--- src/routes.php
+++ src/routes.php
@@ -242,72 +242,75 @@ $app->get('/pakotGains',function ($request, $response, $args){
 $app->get('/cleanAll',function ($request, $response, $args){
 	$conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 	$sql = "
-	DROP TABLE commonuser;
-	CREATE TABLE `commonuser` (
-		`name` text NOT NULL,
-		`email` varchar(255) NOT NULL,
-		`rating` text NOT NULL,
-		`phone` text NOT NULL,
-		`packages` text NOT NULL,
-		`payment` text NOT NULL,
-		`cpf` varchar(255) NOT NULL,
-		`wallet` float(10,2),
-		`id` bigint(20) NOT NULL AUTO_INCREMENT,
-		PRIMARY KEY (`id`),
-		UNIQUE KEY `email` (`email`),
-		UNIQUE KEY `cpf` (`cpf`)
-	);
-		
-	DROP TABLE packages;
-	CREATE TABLE `packages` (
-		`id` bigint(20) NOT NULL AUTO_INCREMENT,
-		`nickname` text NOT NULL,
-		`description` text NOT NULL,
-		`priority` text NOT NULL,
-		`size` text NOT NULL,
-		`weight` text NOT NULL,
-		`sender` text NOT NULL,
-		`deliveryman` text NOT NULL,
-		`price` text NOT NULL,
-		`payment` text NOT NULL,
-		`assigned` text NOT NULL,
-		`dispatched` text NOT NULL,
-		`arrived` text NOT NULL,
-		`canceled` text NOT NULL,
-		`experienceRating` text NOT NULL,
-		`origin` text NOT NULL,
-		`destination` text NOT NULL,
-		`datecreate` text NOT NULL,
-		`dateassigned` text NOT NULL,
-		`datedispatched` text NOT NULL,
-		`datearrived` text NOT NULL,
-		`datecanceled` text NOT NULL,
-		PRIMARY KEY (`id`)
-	);
+		DROP TABLE commonuser;	
+		DROP TABLE packages;
+		DROP TABLE deliveryman;
+		DROP TABLE pakotgain;
 
-	DROP TABLE deliveryman;
-	CREATE TABLE `deliveryman` (
-		`name` text NOT NULL,
-		`email` varchar(255) NOT NULL,
-		`rating` text NOT NULL,
-		`phone` text NOT NULL,
-		`packages` text NOT NULL,
-		`cpf` varchar(255) NOT NULL,
-		`wallet` float(10,2),
-		`id` bigint(20) NOT NULL AUTO_INCREMENT,
-		PRIMARY KEY (`id`),
-		UNIQUE KEY `email` (`email`),
-		UNIQUE KEY `cpf` (`cpf`)
-	);
 
-	DROP TABLE pakotgain;
-	CREATE TABLE `pakotgain` (
-		id int(1) auto_increment,
-		`wallet` float(20,2), 
-		PRIMARY KEY (`id`)
-	);
-	
-	INSERT INTO pakotgain(wallet) values (0)
+		CREATE TABLE `commonuser` (
+			`name` text NOT NULL,
+			`email` varchar(255) NOT NULL,
+			`rating` text NOT NULL,
+			`phone` text NOT NULL,
+			`packages` text NOT NULL,
+			`payment` text NOT NULL,
+			`cpf` varchar(255) NOT NULL,
+			`wallet` float(10,2),
+			`id` bigint(20) NOT NULL AUTO_INCREMENT,
+			PRIMARY KEY (`id`),
+			UNIQUE KEY `email` (`email`),
+			UNIQUE KEY `cpf` (`cpf`)
+		);
+			
+		CREATE TABLE `packages` (
+			`id` bigint(20) NOT NULL AUTO_INCREMENT,
+			`nickname` text NOT NULL,
+			`description` text NOT NULL,
+			`priority` text NOT NULL,
+			`size` text NOT NULL,
+			`weight` text NOT NULL,
+			`sender` text NOT NULL,
+			`deliveryman` text NOT NULL,
+			`price` text NOT NULL,
+			`payment` text NOT NULL,
+			`assigned` text NOT NULL,
+			`dispatched` text NOT NULL,
+			`arrived` text NOT NULL,
+			`canceled` text NOT NULL,
+			`experienceRating` text NOT NULL,
+			`origin` text NOT NULL,
+			`destination` text NOT NULL,
+			`datecreate` text NOT NULL,
+			`dateassigned` text NOT NULL,
+			`datedispatched` text NOT NULL,
+			`datearrived` text NOT NULL,
+			`datecanceled` text NOT NULL,
+			PRIMARY KEY (`id`)
+		);
+
+		CREATE TABLE `deliveryman` (
+			`name` text NOT NULL,
+			`email` varchar(255) NOT NULL,
+			`rating` text NOT NULL,
+			`phone` text NOT NULL,
+			`packages` text NOT NULL,
+			`cpf` varchar(255) NOT NULL,
+			`wallet` float(10,2),
+			`id` bigint(20) NOT NULL AUTO_INCREMENT,
+			PRIMARY KEY (`id`),
+			UNIQUE KEY `email` (`email`),
+			UNIQUE KEY `cpf` (`cpf`)
+		);
+
+		CREATE TABLE `pakotgain` (
+			id int(1) auto_increment,
+			`wallet` float(20,2), 
+			PRIMARY KEY (`id`)
+		);
+
+		INSERT INTO pakotgain(wallet) values (0);
+
 	";
 	
 	$conn->query($sql);
