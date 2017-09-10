3547d2b854bc9622426accae36300587c02a9b93
Unknown <raulfmansur@hotmail.com>
2017-09-10 19:53:18 -0300

    Truncate price in front.

--- src/pakot/functions/DeliveryMan/getAllDeliveryManPackages.php
+++ src/pakot/functions/DeliveryMan/getAllDeliveryManPackages.php
@@ -11,7 +11,7 @@ function getAllDeliveryManPackages($email){
 			array_push($packages, getPackageById($packagesIds[$i]));
 			$packages[count($packages)-1]['flag'] = false;
 			$packages[count($packages)-1]['price'] = intval($packages[count($packages)-1]['price']);
-			$packages[count($packages)-1]['deliveryCut'] = $packages[count($packages)-1]['price'] * DELIVERYMANPERCENTAGE;				
+			$packages[count($packages)-1]['deliveryCut'] = intval($packages[count($packages)-1]['price'] * DELIVERYMANPERCENTAGE);				
 		}
 	}
 	return array_reverse($packages);
